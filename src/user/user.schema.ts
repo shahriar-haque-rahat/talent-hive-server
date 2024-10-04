a1b6112dc77d3c9f98b69d82f9bb11096c884d91
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2024-10-05 01:43:51 +0600

    user

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -3,23 +3,47 @@ import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class User extends Document {
-    @Prop({ required: true, maxlength: 100 })
+    @Prop({ required: true })
     fullName: string;
 
-    @Prop({ required: true, maxlength: 40 })
+    @Prop({ required: true })
     userName: string;
 
-    @Prop({ required: true, maxlength: 100 })
+    @Prop({ required: true })
     email: string;
 
-    @Prop({ required: true, maxlength: 255 })
+    @Prop({ required: true })
     password: string;
 
-    @Prop({ maxlength: 40, default: null })
+    @Prop({ default: null })
     status?: string;
 
-    @Prop({ maxlength: 40, default: null })
+    @Prop({ default: null })
     role?: string;
+
+    @Prop({ default: null })
+    profileImage?: string;
+
+    @Prop({ default: null })
+    coverImage?: string;
+
+    @Prop({ default: null })
+    designation?: string;
+
+    @Prop({ default: null })
+    phoneNumber?: number;
+
+    @Prop({ default: null })
+    about?: string;
+
+    @Prop({ default: null })
+    facebookLink?: string;
+
+    @Prop({ default: null })
+    linkedInLink?: string;
+
+    @Prop({ default: null })
+    cvLink?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
