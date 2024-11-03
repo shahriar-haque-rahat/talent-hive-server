45cded8d445d8f87f89a5c9d1cbdedd9f6e4e219
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2024-11-04 01:03:04 +0600

    axios interceptor

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: '*',
+    origin: process.env.CLIENT_URL,
+    credentials: true,
   });
 
   app.useGlobalPipes(
